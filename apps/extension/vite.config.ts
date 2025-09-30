852c5f1e9e51162373650a7a5cca697b707cfbe9
KTrain5369 <me@git.ktrain5369.dev>
2025-09-30 10:17:05 +1000

    feat(api/ext): add other parts of monorepo
    currently only have a basic JSON API and more or less sample extension

---
+++ apps/extension/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
+
+export default defineConfig({
+  plugins: [
+    crx({ manifest }) // CRXJS auto-parses manifest and wires entries (content scripts, service_worker, pages)
+  ],
+  build: {
+    // optionally tune outDir, rollupOptions if you have multiple HTML pages (popup/options)
+    outDir: "dist"
+  }
+});
