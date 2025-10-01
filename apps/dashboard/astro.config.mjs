9eccc9084ed48d9b6927efdb799ec2e5175841c7
KTrain5369 <me@git.ktrain5369.dev>
2025-10-01 23:06:34 +1000

    feat: add @astrojs/node to dashboard for SSR, add start:dev & dev containers

--- apps/dashboard/astro.config.mjs
+++ apps/dashboard/astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config"
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  server: {
+    port: 5000
+  },
+
+  adapter: node({
+    mode: "standalone"
+  })
+})
