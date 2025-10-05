4f47f394f2ef952b48fdd76abf66ef3f95d223b6
Nitrrine <nitrrine@team.lodine.xyz>
2025-10-05 21:53:07 +0500

    feat(dash): add CF_PAGES_COMMIT_SHA to env schema

--- apps/dashboard/astro.config.mjs
+++ apps/dashboard/astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config"
+import { defineConfig, envField } from "astro/config"
 
 import cloudflare from "@astrojs/cloudflare"
 
@@ -8,6 +8,15 @@ export default defineConfig({
   site: "https://dash.smc.lodine.xyz",
   server: {
     port: 5000
+  },
+  env: {
+    schema: {
+      CF_PAGES_COMMIT_SHA: envField.string({
+        context: "client",
+        access: "public",
+        default: "a1b2c3d"
+      })
+    }
   },
   output: "server",
   adapter: cloudflare()
